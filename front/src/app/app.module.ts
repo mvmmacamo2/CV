01f51545dd0100f8ff83e426df80e6a5557b3dff
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-10-24 07:45:36 -0700

    criando componentes menu(geral ina inicale admin pag)

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
+import { MenugeralComponent } from './menugeral/menugeral.component';
+import { MenuadminComponent } from './menuadmin/menuadmin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaInicialComponent,
+    MenugeralComponent,
+    MenuadminComponent
   ],
   imports: [
     BrowserModule
