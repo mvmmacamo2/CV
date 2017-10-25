c42ab7ad395864e03bb8f19d5db67e087acc5f64
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-10-24 22:45:34 -0700

    importando os Modulos

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,22 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
-import { MenugeralComponent } from './menugeral/menugeral.component';
-import { MenuadminComponent } from './menuadmin/menuadmin.component';
+import {AppComponent} from './app.component';
+import {PaginaInicialComponent} from './pagina-inicial/pagina-inicial.component';
+import {MenugeralComponent} from './menugeral/menugeral.component';
+import {MenuadminComponent} from './menuadmin/menuadmin.component';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PaginaInicialComponent,
-    MenugeralComponent,
-    MenuadminComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        PaginaInicialComponent,
+        MenugeralComponent,
+        MenuadminComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+         MDBBootstrapModule.forRoot()
+    ],
+    schemas: [NO_ERRORS_SCHEMA],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
